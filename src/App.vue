46766c290013f7fb1a82325017feb226bc48bc1f
Murad <M.Schumacher85@mail.ru>
2021-08-01 00:12:39 +0300

    subscribeToUpdate

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+
+  <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center" v-if="preloader">
+   <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+   </svg>
+  </div>
+
+
   <div class="container">
    <div class="w-full my-4"></div>
    <section>
@@ -19,6 +28,15 @@
                placeholder="Например DOGE"
        />
       </div>
+
+      <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap" v-if="ticker">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              {{ticker}}
+            </span>
+      </div>
+      <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+
+
      </div>
     </div>
     <button
@@ -130,17 +148,53 @@
 <script>
  export default {
   name: "App",
+  created() {
+   this.preloader = false
+
+   const tickersData = localStorage.getItem('cryptonomicon-list')
+
+   if(tickersData){
+     this.tickers = JSON.parse(tickersData)
+   }
+
+   this.tickers.forEach(t => this.subscribeToUpdate(t.name))
+  },
 
   data() {
    return {
     ticker: "",
+    coin: "",
     tickers: [],
     sel: null,
-    graph:[]
+    graph:[],
+    preloader: true,
+    coinList:[
+     {name: 'USD'},
+     {name: 'BTC'},
+     {name: 'ETH'},
+     {name: 'DOGE'},
+     {name: 'SHIBA'},
+    ]
    };
   },
 
   methods: {
+   subscribeToUpdate(tickerName){
+    setInterval(async () => {
+     const f = await fetch(
+             `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD,JPY,EUR&api_key=e004f7b2af02f22f7f3ef33c8a7c11a2735db3a71667657f0e827af5114dc28b`
+     )
+     const data = await f.json()
+
+     /*newTicker.price = data.USD*/
+     this.tickers.find(t => t.name == tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
+
+     if(this.sel?.name == tickerName){
+      this.graph.push(data.USD)
+     }
+    },5000)
+
+   },
    add() {
     const currentTicker = {
      name: this.ticker,
@@ -149,21 +203,11 @@
 
     this.tickers.push(currentTicker);
 
-    setInterval(async () => {
-      const f = await fetch(
-              `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD,JPY,EUR&api_key=e004f7b2af02f22f7f3ef33c8a7c11a2735db3a71667657f0e827af5114dc28b`
-      )
-     const data = await f.json()
+    localStorage.setItem('cryptonomicon-list',JSON.stringify(this.tickers))
 
-     /*newTicker.price = data.USD*/
-     this.tickers.find(t => t.name == currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
+    this.subscribeToUpdate(currentTicker.name)
 
-     if(this.sel?.name == currentTicker.name){
-      this.graph.push(data.USD)
-     }
 
-     console.log(data)
-    },5000)
     this.ticker = "";
 
    },
@@ -183,6 +227,9 @@
    handleDelete(tickerToRemove) {
     this.tickers = this.tickers.filter(t => t !== tickerToRemove);
    },
+
+
+
   }
  };
 </script>
